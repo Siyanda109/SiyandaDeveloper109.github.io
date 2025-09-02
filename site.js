987015bd22fab47acf2ed8821bf04c0ae1b9ce41
Siyanda109 <syandamalex00@gmail.com>
2025-09-02 03:11:14 +0200

    Update site.js

---
+++ site.js
@@ -0,0 +1,16 @@
+         const closedb = document.getElementById('close')
+          const learn = document.getElementById('learn')
+          const sec1 = document.getElementById('sec1')
+          const lang = document.getElementById('lang')
+
+
+          closedb.addEventListener('click',()=>{
+            lang.style.display = 'none'
+            sec1.style.filter = 'none'
+
+          })
+
+          learn.addEventListener('click',()=>{
+            lang.style.display = 'flex'
+            sec1.style.filter = 'blur(20px)'
+          })
